3f96b527a405d3240787beecbc1f725edc7d1640
Araninyo <aran_rashid@hotmail.com>
2017-01-20 15:36:32 +0100

    Fixed the issue with global variables

--- app/src/main/java/com/example/sam/vo2max/MainActivity.java
+++ app/src/main/java/com/example/sam/vo2max/MainActivity.java
@@ -5,24 +5,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE1 = "com.example.sam.vo2max.MESSAGE1";
+    public final static String EXTRA_MESSAGE2 = "com.example.sam.vo2max.MESSAGE2";
+    public final static String EXTRA_MESSAGE3 = "com.example.sam.vo2max.MESSAGE3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        Button NewMeasurement = (Button) findViewById(R.id.StartNewMeasurementID);
-        NewMeasurement.setOnClickListener(new View.OnClickListener() {
+        Button btnNewMeasurement = (Button) findViewById(R.id.StartNewMeasurementID);
+        btnNewMeasurement.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                Intent intentone = new Intent(MainActivity.this, TheMeasurementActivity.class);
-                startActivity(intentone);
+                Intent intent = new Intent(MainActivity.this, TheMeasurementActivity.class);
+
+                EditText etName = (EditText) findViewById(R.id.NameEditTextID);
+                EditText etAge = (EditText) findViewById(R.id.AgeEditTextID);
+                EditText etWeight = (EditText) findViewById(R.id.WeightEditTextID);
+
+                String sMessage, sAge, sWeight;
+
+                sMessage = etName.getText().toString();
+                sAge = etAge.getText().toString();
+                sWeight = etWeight.getText().toString();
+
+                intent.putExtra(EXTRA_MESSAGE1, sMessage);
+                intent.putExtra(EXTRA_MESSAGE2, sAge);
+                intent.putExtra(EXTRA_MESSAGE3, sWeight);
+
+                startActivity(intent);
             }
         });
 
     }
+
 }
--- app/src/main/java/com/example/sam/vo2max/TheMeasurementActivity.java
+++ app/src/main/java/com/example/sam/vo2max/TheMeasurementActivity.java
@@ -2,13 +2,27 @@ package com.example.sam.vo2max;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.widget.TextView;
 
 public class TheMeasurementActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_the_measurement);
+
+        Intent intent = getIntent();
+
+        String sName = intent.getStringExtra(MainActivity.EXTRA_MESSAGE1);
+        String sAge = intent.getStringExtra(MainActivity.EXTRA_MESSAGE2);
+        String sWeight = intent.getStringExtra(MainActivity.EXTRA_MESSAGE3);
+
+        //Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+
+        textView.setText("Your name is " + sName + " and you are " + sAge + " years old and you weight" + sWeight + " kg");
+        setContentView(textView);
 
     }
 }
