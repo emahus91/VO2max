07d135fad12e9736da974d4f7e77e6cc71068803
Araninyo <aran_rashid@hotmail.com>
2017-02-13 00:18:22 +0100

    Fatal try to create the Database-activity, code seems right but that I am missing some small thing.. when I press the submit button, the app crashes and it seems a lot of other people have the same problem, it is probably something wrong with how you declare buttons in android studio and their functions

--- app/src/main/java/com/example/sam/vo2max/MainActivity.java
+++ app/src/main/java/com/example/sam/vo2max/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.sam.vo2max;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText ContactName, ContactAge, ContactWeight;
+    Context context = this;
+    UserDbHelper userDbHelper;
+    SQLiteDatabase sqLiteDatabase;
 
     public final static String EXTRA_MESSAGE1 = "com.example.sam.vo2max.MESSAGE1";
     public final static String EXTRA_MESSAGE2 = "com.example.sam.vo2max.MESSAGE2";
@@ -17,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ContactName = (EditText) findViewById(R.id.NameEditTextID);
+        ContactName = (EditText) findViewById(R.id.AgeEditTextID);
+        ContactName = (EditText) findViewById(R.id.WeightEditTextID);
 
         Button btnNewMeasurement = (Button) findViewById(R.id.StartNewMeasurementID);
         btnNewMeasurement.setOnClickListener(new View.OnClickListener() {
@@ -43,6 +54,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void addContact(View view)
+    {
+        String name = ContactName.getText().toString();
+        String age = ContactAge.getText().toString();
+        String weight = ContactWeight.getText().toString();
+        userDbHelper = new UserDbHelper(context);
+        sqLiteDatabase = userDbHelper.getWritableDatabase();
+        userDbHelper.addInformations(name, age, weight, sqLiteDatabase);
+            Toast.makeText(getBaseContext(),"Data Saved",Toast.LENGTH_LONG).show();
+                userDbHelper.close();
+    }
+
+    public void viewContact(View view)
+    {
+        Intent intent = new Intent(this, DataListActivity.class);
+        startActivity(intent);
     }
 
 }
